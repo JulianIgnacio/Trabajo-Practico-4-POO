b6e8f77221296226ac1207f11ecb2c0eaf759295
JulianIgnacio <montenegrojulianig@gmail.com>
2023-04-03 18:44:55 -0300

    actualizando

---
+++ js/Ejercicio6.js
@@ -0,0 +1,9 @@
+class Libro {
+    constructor(ISBN,titulo,autor,numeroPag){
+        this.ISBN = ISBN;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPag = numeroPag;
+    }
+    
+}
